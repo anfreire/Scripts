73b4fc0215bd8ac779fe3dbb8863425cea972ea1
André Freire Ferreira <anfreire.dev@gmail.com>
2024-11-21 12:10:34 +0000

    Update title-generator.py

--- Python/title-generator.py
+++ Python/title-generator.py
@@ -1,28 +1,49 @@
 import argparse
 import pyperclip
 
+DEFAULT_WIDTH = 80
 
-def copy_to_clipboard(text):
-    pyperclip.copy(text)
+DEFAULT_HEIGHT = 3
+POSSIBLE_HEIGHTS = (3, 5)
 
 
-def generate_comment(title, width, upper, height, alt_style):
-    title = title.upper() if upper else title
+def copy_to_clipboard(generated_title: str):
+    pyperclip.copy(generated_title)
+
+
+def generate_title_comment(
+    title: list[str],
+    width: int = DEFAULT_WIDTH,
+    height: int = DEFAULT_HEIGHT,
+    upper: bool = None,
+    alt_style: bool = None,
+):
+    title_str = " ".join(title)
+
+    if upper:
+        title_str = title_str.upper()
+
     if alt_style:
-        border = "#" * width
+        top_line = "#" * width
+
+        middle_line = "#" + title_str.center(width - 2) + "#"
+
+        bottom_line = top_line
+
         padding = "#" + " " * (width - 2) + "#"
-        title_line = "#" + title.center(width - 2) + "#"
     else:
-        border = "/**" + "*" * (width - 3)
-        padding = " *" + " " * (width - 3) + "*"
-        title_line = " *" + title.center(width - 3) + "*"
-        border_end = " *" + "*" * (width - 3) + "*/"
+        top_line = "/**" + "*" * (width - 4) + " "
 
-    lines = [border, title_line, border if alt_style else border_end]
+        middle_line = " *" + title_str.center(width - 4) + "* "
 
-    if height == 5:
-        lines.insert(1, padding)
-        lines.insert(3, padding)
+        bottom_line = " " + "*" * (width - 3) + "*/"
+
+        padding = " *" + " " * (width - 4) + "* "
+
+    if height == 3:
+        lines = [top_line, middle_line, bottom_line]
+    else:
+        lines = [top_line, padding, middle_line, padding, bottom_line]
 
     return "\n".join(lines)
 
@@ -32,16 +53,20 @@ def main():
         description="Generate a title in a code comment style.",
     )
 
-    parser.add_argument("title", type=str, help="The title to be generated")
+    parser.add_argument("title", type=str, nargs="+", help="The title to be generated")
     parser.add_argument(
-        "-x", "--width", type=int, default=80, help="The width of the comment"
+        "-x",
+        "--width",
+        type=int,
+        default=DEFAULT_WIDTH,
+        help="The width of the comment",
     )
     parser.add_argument(
         "-y",
         "--height",
         type=int,
-        default=3,
-        choices=[3, 5],
+        default=DEFAULT_HEIGHT,
+        choices=POSSIBLE_HEIGHTS,
         help="The height of the comment",
     )
     parser.add_argument(
@@ -59,7 +84,9 @@ def main():
 
     args = parser.parse_args()
 
-    comment = generate_comment(args.title, args.width, args.upper, args.height, args.alt)
+    comment = generate_title_comment(
+        args.title, args.width, args.height, args.upper, args.alt
+    )
     print(comment)
     copy_to_clipboard(comment)
 
